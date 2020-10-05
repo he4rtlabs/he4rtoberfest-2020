67b3d9904b366bd9ce29c7b3d560ebd752e2cfc6
rafaelmendes1 <rarafafa3245@gmail.com>
2020-10-05 17:46:30 -0300

    Finalizando o desafio

---
+++ challenges/1-media-alunos/java/MediaAritmetica.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MediaAritmetica {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double num, num2;
+		num = sc.nextDouble();
+		num2 = sc.nextDouble();
+		
+		System.out.println((num+num2)/2);
+		
+		sc.close();
+
+	}
+
+}
