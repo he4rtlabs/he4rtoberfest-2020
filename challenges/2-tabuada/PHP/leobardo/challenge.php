763e90cd08fc06763e33e13b842ce7bc63b72193
LeoBardo <45073941+LeoBardineo@users.noreply.github.com>
2020-10-05 19:43:47 -0300

    Finalizando desafio

---
+++ challenges/2-tabuada/PHP/leobardo/challenge.php
@@ -0,0 +1,17 @@
+<?php
+
+if(!is_numeric($argv[1])){
+    echo 'Passe um número como argumento.';
+    exit;
+}
+
+$limite = (int)$argv[1];
+
+for ($multiplicando = 1; $multiplicando <= $limite; $multiplicando++) {
+    echo '●▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬●'.PHP_EOL;
+    echo "Tábuada do número $multiplicando".PHP_EOL;
+    for ($multiplicador = 1; $multiplicador <= 10; $multiplicador++) { 
+        echo "$multiplicando x $multiplicador = ".$multiplicando * $multiplicador.PHP_EOL;
+    }
+    echo PHP_EOL;
+}
---
+++ challenges/2-tabuada/PHP/leobardo/readme.md
@@ -0,0 +1,20 @@
+# Submissão de Exercicio
+
+**Exercicio:** 2 - Tabuada 
+
+**Nickname:** leobardo
+
+**Nível Técnico:** - Estudante
+
+**Twitter**: https://twitter.com/lbardo0 (opcional)
+
+**Dificuldade de Resolução:** - Baixa
+
+**Comentários:** Nesse desafio eu achei que ... (Opcional)
+
+**Como rodar o desafio**: 
+
+Use o comando abaixo: 
+```bash
+php challenge.php argv1
+```
