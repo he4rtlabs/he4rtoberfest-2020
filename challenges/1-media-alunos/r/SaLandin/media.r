732ccf9a0124832ef3e857ab594f0b19c41e4d9f
SaLandini <62630050+SaLandini@users.noreply.github.com>
2020-10-05 10:48:09 -0300

    [Challenge #1]Media dos Alunos com R

---
+++ challenges/1-media-alunos/r/SaLandin/media.r
@@ -0,0 +1,24 @@
+
+calc <- function(nota1, nota2){
+  print("Bem vindo a calculadora de medias com R")
+  nota1 <- readline(prompt = "Qual a sua primeira nota? ")
+  nota2 <- readline(prompt = "Qual a sua segunda nota? ")
+  
+  print("Processando.")
+  print("Processando..")
+  print("Processando...")
+  print("Processando..")
+  print("Processando.")
+  
+  nota1 <- as.integer(nota1)
+  nota2 <- as.integer(nota2)
+  
+  resp <- (nota1+nota2)/2
+  
+  resp <- as.character(resp)
+  
+  print("A sua média é:")
+  print(resp)
+}
+
+calc()
