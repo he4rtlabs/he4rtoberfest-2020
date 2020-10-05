e22a3ca2d13fe0d0fef7713e75618b7c1e2bd9b5
evertoont <evertoontcarmo@gmail.com>
2020-10-05 15:36:18 -0300

    Deleta arquivos do desafio 0 e 1

--- challenges/1-media-alunos/python/evertoont/challenge.py
+++
@@ -1,6 +0,0 @@
-primeira_nota = float(input('Digite a primeira nota: '))
-segunda_nota = float(input('Digite a segunda nota: '))
-
-media = (primeira_nota + segunda_nota) / 2
-
-print(f'\nMédia: {media:.2f}')
