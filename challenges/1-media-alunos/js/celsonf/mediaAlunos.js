f22a2dcb5a1f20a22be50cc024bfbe218e46012a
Celson F. ROD <celson338540@gmail.com>
2020-10-05 20:42:18 -0300

    Update challenges/1-media-alunos/js/celsonf/mediaAlunos.js
    Co-authored-by: @he4rt <61126031+he4rtlabs@users.noreply.github.com>

--- challenges/1-media-alunos/js/celsonf/mediaAlunos.js
+++ challenges/1-media-alunos/js/celsonf/mediaAlunos.js
@@ -1,6 +1,6 @@
 function mediaAlunos(primeiraNota,segundaNota){
     let media;
     return media = (primeiraNota + segundaNota) / 2;
-    }
+}
     
     console.log(`Média -> ${mediaAlunos(22,53)}`);
