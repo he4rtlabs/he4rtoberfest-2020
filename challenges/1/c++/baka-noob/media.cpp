62d64f1a87e080d45555497575b98a4a85156623
Barbara Braga <barbarabgual@hotmail.com>
2020-10-04 23:50:30 -0300

    media.cpp

---
+++ challenges/1/c++/baka-noob/media.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    float nota1, nota2, media;
+
+    cin >> nota1 >> nota2;
+    media = (nota1 + nota2)/2;
+
+    cout << "Média -> " << media << endl;
+
+    return 0;
+}
