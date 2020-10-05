e8e26d6c2e3d8ab16f1e6468600fc6cd83853769
João Godinho <godinhojoao2003@gmail.com>
2020-10-05 01:40:17 -0300

    melhoria no codigo

--- challenges/2-tabuada/js/godinhojoao/tabuada.js
+++ challenges/2-tabuada/js/godinhojoao/tabuada.js
@@ -4,16 +4,9 @@ function multiplicationTable(number) {
     for (let i = 1; i <= number; i++) {
         console.log('===========================');
         console.log(`A tabuada do numero ${i} é:`);
-        console.log(`${i} * 1 = ${i * 1}`);
-        console.log(`${i} * 2 = ${i * 2}`);
-        console.log(`${i} * 3 = ${i * 3}`);
-        console.log(`${i} * 4 = ${i * 4}`);
-        console.log(`${i} * 5 = ${i * 5}`);
-        console.log(`${i} * 6 = ${i * 6}`);
-        console.log(`${i} * 7 = ${i * 7}`);
-        console.log(`${i} * 8 = ${i * 8}`);
-        console.log(`${i} * 9 = ${i * 9}`);
-        console.log(`${i} * 10 = ${i * 10}`);
+        for (let j = 1; j <= 10; j++) {
+            console.log(`${i} x ${j} = ${i * j}`);
+        }
         console.log('===========================');
     };
 };
