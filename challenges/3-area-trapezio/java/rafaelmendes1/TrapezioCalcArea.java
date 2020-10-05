e3a72d1337825cc52df28eba660591d4fd36251a
rafaelmendes1 <rarafafa3245@gmail.com>
2020-10-05 18:22:32 -0300

    Trapezio

---
+++ challenges/3-area-trapezio/java/rafaelmendes1/TrapezioCalcArea.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TrapezioCalcArea {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double baseMaior, baseMenor, altura;
+		baseMaior = sc.nextDouble();
+		baseMenor = sc.nextDouble();
+		altura = sc.nextDouble();
+		
+		System.out.println("A área do trapézio é: "+ (baseMaior + baseMenor)*altura/2);
+		
+		sc.close();
+
+	}
+
+}
