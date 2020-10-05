4a7c1151df1315e4219475bb92ca43dc50158627
JulioC16 <julio20.cesar02@gmail.com>
2020-10-05 16:35:29 -0300

    finalizado

---
+++ challenges/3-area-trapezio/php/jcesar/trapezio.php
@@ -0,0 +1,22 @@
+<?php
+if (sizeof($argv) < 4)
+{
+    echo "FALTAM ARGUMENTOS";
+    exit(); 
+}
+
+$baseMaior = $argv[1];
+$baseMenor = $argv[2];
+$altura = $argv[3]; 
+
+
+if (!is_numeric($baseMaior) OR !is_numeric($baseMenor) OR !is_numeric($altura))
+{
+    echo "INSERA 3 NÚMEROS POR FAVOR";
+    exit();
+}
+
+
+$area = ($baseMaior + $baseMenor) * $altura / 2;
+
+echo "A área do trapézio é: {$area}";
