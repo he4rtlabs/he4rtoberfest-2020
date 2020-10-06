3b74047ef0042d5f00ed50047ef0723db28264bd
vitogd <victorfelipe.guedes@hotmail.com>
2020-10-06 18:09:35 -0300

    [Challenge #2] Tabuada

---
+++ challenges/2-tabuada/cpp/vitogd/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void calcularTabuada(int p) {
+  for (int i = 1; i <= p; i++) {  
+    std::cout << "\n===========================";
+    std::cout << "\nA tabuada do numero " << i << " eh:\n" << std::endl;
+
+    for (int j = 1; j <= 10; j++)
+      std::cout << i << " x " << j << " = " << i * j << std::endl;
+  }
+  std::cout << "===========================";
+}
+
+int main() {
+  int parada;
+
+  std::cout << "Digite a parada da tabuada: ";
+  std::cin >> parada;
+
+  calcularTabuada(parada);
+  return 0;
+}
