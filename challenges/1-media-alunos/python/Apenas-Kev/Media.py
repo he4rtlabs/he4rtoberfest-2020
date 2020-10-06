e85d89ca3816c0ff14fe1c007ef3edde1ebe47de
Kelwin Karan <kelwinkaran@gmail.com>
2020-10-05 22:28:17 -0300

    correção de input não numerico
    impede o usuário de colocar informações não numéricas na aplicação

--- challenges/1-media-alunos/python/Apenas-Kev/Media.py
+++ challenges/1-media-alunos/python/Apenas-Kev/Media.py
@@ -1,4 +1,9 @@
-media_1 = float(input('insira a primeira nota: '))
-media_2 = float(input('insira a segunda nota: '))
-
-print(f'A media do aluno em um bimestre foi {(media_2 + media_1) / 2 :.1f}')
+while True:
+    try:
+        media_1 = float(input('insira a primeira nota: '))
+        media_2 = float(input('insira a segunda nota: '))
+        print(f'A media do aluno em um bimestre foi {(media_2 + media_1) / 2 :.1f}')
+        break;
+    except ValueError:
+        print('Por favor insira apenas numeros')
+        
