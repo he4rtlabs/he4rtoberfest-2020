c718770fba38da273c1a1ae976b0826a187faab7
afpp3 <afelipp3@gmail.com>
2020-10-06 15:14:38 -0300

    Modificando desafio para aceitar varios numeros

--- challenges/1-media-alunos/js/afpp3/media.js
+++ challenges/1-media-alunos/js/afpp3/media.js
@@ -1,6 +1,22 @@
-function averageCalc(nota1, nota2) {
-    const media = (nota1 + nota2) / 2
-    return console.log(`Média => ${media}`)
+const notas = process.argv.slice(2)
+
+function calculaMedia(notas) {
+  // Verifica se cada argumento passado é um número
+  const isNumber = notas.every(nota => !isNaN(nota));
+
+  if (isNumber) {
+
+    const somaNotas = notas.reduce((acc, num) => {
+      // Transforma o valor atual do array que no momento é uma string em um número para realizar a soma
+      return acc + Number(num)
+    }, 0)
+
+    const media = somaNotas / notas.length
+    console.log(`Média => ${media.toFixed(2)}`)
+  } else {
+    console.log('❌ Por favor, digite apenas números como argumento')
+  }
 }
 
-averageCalc(22, 53)
+// Pega os argumentos digitados no console e transforma em um array de string
+calculaMedia(notas)
--- challenges/1-media-alunos/js/afpp3/readme.md
+++ challenges/1-media-alunos/js/afpp3/readme.md
@@ -10,10 +10,24 @@
 
 **Dificuldade de Resolução:** - Baixa
 
+**Comentários:** Achei o desafio bem interessante, pois me proporcionou aprender coisas novas que
+não conhecia, ou não tinha costume de usar, como por exempo receber os dados pelo usuário no terminal ao executar
+o script do arquivo em javascript.
+
 **Como rodar o desafio**:
 
 Use o comando abaixo:
 
 ```bash
-node media.js
+node media.js arg1, arg2 arg3...
 ```
+
+**Exemplo**:
+
+```bash
+node media.js 10 5 7
+```
+
+O exemplo irá retornar: Média => 7.33
+
+Lembrando que os valores são as notas que deseja calcular a média
