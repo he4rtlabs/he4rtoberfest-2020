a19fb1d99302c330c9fa2b48acde9e2059ab3653
yzPeedro <pedrocruzpessoa16@gmail.com>
2020-10-06 19:45:03 -0300

    Finalizando o desafio

--- challenges/3-area-trapezio/php/yzpeedro/challenge.php
+++ challenges/3-area-trapezio/php/yzpeedro/challenge.php
@@ -1,19 +1,18 @@
 <?php
 	
-	//Recebe valores do terminal e coloca dentro de suas respectivas variáveis
-	$bMaior = $argv[1];
-	$bMenor = $argv[2];
-	$altura = $argv[3];
+//Recebe valores do terminal e coloca dentro de suas respectivas variáveis
+$bMaior = $argv[1];
+$bMenor = $argv[2];
+$altura = $argv[3];
 
-	//Verifica se o valor inserido é númerico
-	if (!is_numeric($bMaior) || !is_numeric($bMenor) || !is_numeric($altura)) {
-		//retorna relatório de erro
-	    echo PHP_EOL . 'Por favor, insira um valor numérico!' . PHP_EOL;
-	    return false;
-	} else {
-		//equação efetuada com sucesso
-		$area = (($bMaior + $bMenor) * $altura) / 2;
-		echo PHP_EOL . 'Resultado: ' . $area . PHP_EOL;
-	}
-
+//Verifica se o valor inserido é númerico
+if (!is_numeric($bMaior) || !is_numeric($bMenor) || !is_numeric($altura)) {
+	//retorna relatório de erro
+	echo PHP_EOL . 'Por favor, insira um valor numérico!' . PHP_EOL;
+	return false;
+} else {
+	//equação efetuada com sucesso
+	$area = (($bMaior + $bMenor) * $altura) / 2;
+	echo PHP_EOL . 'Resultado: ' . $area . PHP_EOL;
+}
 	
