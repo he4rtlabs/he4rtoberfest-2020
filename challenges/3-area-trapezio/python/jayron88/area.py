bcbe1fd9786f8e95e1c210490a379c0a4f2e8235
jayron88 <jayron@hotmail.com.br>
2020-10-06 13:10:35 -0300

    Update area.py

--- challenges/3-area-trapezio/python/jayron88/area.py
+++ challenges/3-area-trapezio/python/jayron88/area.py
@@ -1,18 +1,9 @@
 while True:
     try:
-        b_maior = int(input('Digite um número: '))
+        b_maior = int(input('Digite o comprimeto da base Maior: '))
+        b_menor = int(input('Digite o comprimeto da base Menor:'))
+        altura = int(input('Digite a altura: '))
     except (ValueError, TypeError):
         print('Digite apenas números!')
     else:
-        try:
-            b_menor = int(input('Digite um número: '))
-        except(ValueError, TypeError):
-            print('Digite apenas números!')
-        else:
-            try:
-                altura = int(input('Digite um número: '))
-            except(ValueError, TypeError):
-                print('Digite apenas números!')
-            else:
-                print(f'A área do trapézio é: {(b_maior + b_menor) * altura / 2}')
-            
+        print(f'A área do trapézio é: {(b_maior + b_menor) * altura / 2}')
