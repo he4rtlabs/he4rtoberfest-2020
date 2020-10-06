dc9e830f44e0df522f0309752223050fb0acb5bf
GuilhermeSouza01 <ghostgui60@gmail.com>
2020-10-05 21:43:09 -0300

    Corrigindo os problemas

---
+++ challenges/1-media-alunos/js/guilhermesouza/average.js
@@ -0,0 +1,10 @@
+
+const  studentAverage =(value1, value2) => {
+    return console.log((value1 + value2)/2);
+};
+
+const firstValue = 22;
+
+const secondValue = 53;
+
+studentAverage(firstValue, secondValue);
