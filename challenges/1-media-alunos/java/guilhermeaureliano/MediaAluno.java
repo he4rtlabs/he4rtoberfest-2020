1aa876d5c82b3211dddf301b500cecc1375b3106
Guilherme Aureliano <49173189+GuilhermeAureliano@users.noreply.github.com>
2020-10-05 22:37:10 -0300

    Adicionando novas funcionalidades

--- challenges/1-media-alunos/java/guilhermeaureliano/MediaAluno.java
+++ challenges/1-media-alunos/java/guilhermeaureliano/MediaAluno.java
@@ -4,16 +4,25 @@ public class MediaAluno {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("Informe sua primeira nota: ");
-		double notaA = input.nextDouble(); 
+		System.out.print("Informe a quantidade de notas que você quer: ");
+		int x = input.nextInt();
+		double somaNotas = 0;
+		int contNotas = 0;
+		try {
+			for(int i = 0; i < x; i++) {
+				System.out.print("Digite sua " + (i+1) + "°" + " nota: ");
+				double nota = input.nextDouble();
+				somaNotas += nota;
+				contNotas ++; // Quantidade de notas válidas
+			}	
+		} catch (Exception ex) {
+			System.out.println("Programa encerrado pois o valor digitado não é um número! \n");
+		}
 		
-		System.out.print("Agora, informe sua segunda nota: ");
-		double notaB = input.nextDouble();
+		double mediaNotas = somaNotas/contNotas;
+		System.out.println("Sua média é: " + mediaNotas);
 		
-		// Calcula a média aritmética
-		double media = (notaA + notaB) / 2;
-		
-		System.out.printf("Sua média é: %.2f", media);
 		input.close();
 	}
+	
 }
