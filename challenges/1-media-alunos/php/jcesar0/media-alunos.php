ad4040eba60c1a5fc479d1daba40ce8806d9e690
JulioC16 <julio20.cesar02@gmail.com>
2020-10-05 21:37:32 -0300

    desafio

--- challenges/1-media-alunos/php/jcesar0/media-alunos.php
+++ challenges/1-media-alunos/php/jcesar0/media-alunos.php
@@ -1,7 +1,19 @@
 <?php
-$notaAlunoUm = $argv[1];
-$notaAlunoDois = $argv[2];
+$pontoInicial = $argv;
 
-$mediaAlunos = ($notaAlunoUm + $notaAlunoDois) / 2;
+$notas = [];
 
-echo "Média -> {$mediaAlunos}";
+foreach ($pontoInicial as $nota) {
+    if (!is_numeric($nota))
+    {
+        continue;
+    }
+    
+    array_push($notas, $nota);
+}
+
+$quantidadeNotas = count($notas);
+
+$mediaAluno = array_sum($notas) / $quantidadeNotas;
+
+echo "A média do aluno foi de {$mediaAluno}";
