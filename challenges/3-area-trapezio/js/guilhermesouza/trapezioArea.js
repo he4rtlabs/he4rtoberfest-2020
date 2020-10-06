a4afa37ec21234ced3916e52c5a181f61dfc278c
GuilhermeSouza01 <ghostgui60@gmail.com>
2020-10-06 10:18:35 -0300

    Finalizando desafio.

---
+++ challenges/3-area-trapezio/js/guilhermesouza/trapezioArea.js
@@ -0,0 +1,21 @@
+const prompt = require('prompt-sync')();
+
+function traparea(baseMaior, baseMenor, altura) {
+    for(let testCase of arguments){
+        if(typeof testCase !== 'number'){
+            return console.log(`${testCase} Não é um numero, por favor use apenas numeros!`);
+        }
+    } 
+    const areaValue = ((baseMaior + baseMenor) * altura) / 2;
+    if(isNaN(areaValue)){
+        return console.log('Ocorreu um erro, por favor digite apenas numeros!');
+    }
+
+    return console.log(`A área do trapezio é: ${areaValue}`);
+}
+
+const baseMaior = parseInt(prompt('Digite a base maior: '));
+const baseMenor = parseInt(prompt('Digite a base menor: '));
+const altura = parseInt(prompt('Digite a altura: '));
+
+traparea(baseMaior, baseMenor, altura);
