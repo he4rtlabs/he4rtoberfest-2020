54dce9de2e89a3560d5abf78f2de8c70e1401c87
wesleyrcb <wesley.batista@ccc.ufcg.edu.br>
2020-10-06 17:21:54 -0300

    Finalizando o desafio

---
+++ challenges/8-fibonacci/java/wesleyrcb/Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int input;
+		int actual = 0, previous1 = 0, previous2 = 1;
+
+		System.out.print("Digite um número: ");
+		input = scan.nextInt();
+
+		for(int i = 0; i < input; i++){
+			System.out.print(actual + " ");
+			int aux = actual;
+			actual = previous1 + previous2;
+			previous1 = aux;
+			previous2 = actual;
+		}
+	}
+}
