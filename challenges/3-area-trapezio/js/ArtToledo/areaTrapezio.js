cba7278a2bd9376347a67379ae415473d7e865b1
ArtToledo <arthur.toledo741@gmail.com>
2020-10-06 14:46:07 -0300

    Finalizando o desafio

--- challenges/3-area-trapezio/js/ArtToledo/areaTrapezio.js
+++ challenges/3-area-trapezio/js/ArtToledo/areaTrapezio.js
@@ -1,6 +1,10 @@
+const largerBase = process.argv[2];
+const smallerBase = process.argv[3];
+const height = process.argv[4];
+
 const calculateTrapezoidArea = (largerBase, smallerBase, height) => {
   try {
-    if (isNaN(largerBase || smallerBase || height)) {
+    if (isNaN(largerBase && smallerBase && height)) {
       throw 'Apenas números são aceitos, altere os valores e tente novamente!';
     }
 
@@ -10,7 +14,5 @@ const calculateTrapezoidArea = (largerBase, smallerBase, height) => {
   }
 };
 
-//erro no parametro
-calculateTrapezoidArea('exception error', 20, 1.8);
-//sucesso, apenas numeros passados
-calculateTrapezoidArea(8, 3, 5);
+//chamando a funcao passando os parametros enviados no comando do node
+calculateTrapezoidArea(largerBase, smallerBase, height);
