b848502422fecfd422359179d723715a9849d701
GuilhermeSouza01 <ghostgui60@gmail.com>
2020-10-05 23:00:20 -0300

    Alteração feita para 'n' notas de alunos

--- challenges/1-media-alunos/js/guilhermesouza/average.js
+++ challenges/1-media-alunos/js/guilhermesouza/average.js
@@ -1,10 +1,18 @@
+const prompt = require('prompt-sync')();
 
-const  studentAverage =(value1, value2) => {
-    return console.log((value1 + value2)/2);
+const number = parseInt(prompt('Digite a quantidade de notas para o aluno(Apenas numeros):'));
+let values = new Array(number);
+for(let accumulator =1; accumulator<=number; accumulator++){
+    values[accumulator] = parseInt(prompt(`Valor${accumulator}:`));
+}
+
+let sum = 0;
+    for(let accumulator in values){
+        sum+=values[accumulator];
+    }
+
+const  studentAverage =(sum,number) => {
+    return console.log((sum)/number);
 };
 
-const firstValue = 22;
-
-const secondValue = 53;
-
-studentAverage(firstValue, secondValue);
+studentAverage(sum, number);
