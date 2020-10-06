6e3684be4281ecc2090c682333c170a5cf8f4a3d
yzPeedro <pedrocruzpessoa16@gmail.com>
2020-10-06 19:34:43 -0300

    Finalizando o desafio

--- challenges/3-area-trapezio/php/yzpeedro/challenge.php
+++ challenges/3-area-trapezio/php/yzpeedro/challenge.php
@@ -7,13 +7,10 @@
 
 	//Verifica se o valor inserido é númerico
 	if (!is_numeric($bMaior) || !is_numeric($bMenor) || !is_numeric($altura)) {
-
 		//retorna relatório de erro
 	    echo PHP_EOL . 'Por favor, insira um valor numérico!' . PHP_EOL;
 	    return false;
-
-	}else{
-
+	} else {
 		//equação efetuada com sucesso
 		$area = (($bMaior + $bMenor) * $altura) / 2;
 		echo PHP_EOL . 'Resultado: ' . $area . PHP_EOL;
