cff9548ab326b24cc2bcba38d2967ca524ff27ef
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-10-06 22:39:44 -0300

    Tratamento de erros na entrada de valores

--- challenges/3-area-trapezio/python/jvhk/area-trapezio.py
+++ challenges/3-area-trapezio/python/jvhk/area-trapezio.py
@@ -1,6 +1,17 @@
-baseMaior = float(input("Digite o valor da base maior do trapézio: "))
-baseMenor = float(input("Digite o valor da base menor do trapézio: "))
-altura = float(input("Digite o valor da altura do trapézio: "))
+try:
+    baseMaior = float(input("Digite o valor da base maior do trapézio: "))
+except ValueError:
+    baseMaior = float(input("O valor digitado não é um número, digite um número para a base maior do trapézio: "))
+
+try:
+    baseMenor = float(input("Digite o valor da base menor do trapézio: "))
+except ValueError:
+    baseMenor = float(input("O valor digitado não é um número, digite um número para a base menor do trapézio: "))
+
+try:
+    altura = float(input("Digite o valor da altura do trapézio: "))
+except ValueError:
+    altura = float(input("O valor digitado não é um número, digite um número para a altura do trapézio: "))
 
 Area = (baseMaior + baseMenor) * altura / 2
 
