5000933e08587146522bf5886fb4f4bf0670a05b
Celson F. ROD <celson338540@gmail.com>
2020-10-06 21:58:40 -0300

    Update with corrections

--- challenges/8-fibonacci/js/celsonf/fibonacci.js
+++ challenges/8-fibonacci/js/celsonf/fibonacci.js
@@ -1,16 +1,17 @@
 function fibonacci(enternumber)
 {
-   let firstNum = 0 , secondNum = 1, nextNum;
+   let firstNum = 0 , secondNum = 1, nextNum, arr = [];
 
    for (let index = 1; index <= enternumber; index++){
-       console.log(firstNum);
+       arr[index] = firstNum;
        nextNum = firstNum + secondNum;
        firstNum = secondNum;
        secondNum = nextNum;
    }
-
+   
+   return arr.join(" ");
 }
 
-console.log(fibonacci(5));
+console.log(` ${fibonacci(5)} `);
 
-console.log(fibonacci(10));
+console.log(` ${fibonacci(10)} `);
