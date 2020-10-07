c44dba1e824036d4665559a3c1850320dfdf7baf
Gustavo Morais <54909865+GustavoVMO@users.noreply.github.com>
2020-10-07 11:02:52 -0300

    Create media.py
    Alterações pedidas

---
+++ challenges/1-media-alunos/python/GustavoVMO/media.py
@@ -0,0 +1,9 @@
+print("Bom dia/tarde/noite. Tudo bem?!")
+print("Vamos calcular a media de duas notas!")
+
+notaUm = float(input("Digite a primeira nota"))
+notaDois = float(input("Digite a segunda nota"))
+
+media = (notaUm + notaDois) / 2
+
+print("A média das duas notas é: {:.1f}".format(media))
