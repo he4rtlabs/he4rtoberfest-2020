6a04692fefb693c035ffc19a507c65c2557f1f03
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2020-10-06 21:29:13 -0300

    Finalizando o desafio

---
+++ challenges/2-tabuada/java/brandaogabriel/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int valor = sc.nextInt();
+		
+		System.out.println("===========================");
+		
+		for(int i = 1; i <= valor; i++) {
+			System.out.println("A tabuada do numero " + i + " é: ");	
+			for(int j = 1; j < 11; j++) {
+				System.out.println(i + " * " + j + " = " + i*j);			
+			}
+			System.out.println("===========================");
+		}
+		
+		sc.close();
+	}
+
+}
+
