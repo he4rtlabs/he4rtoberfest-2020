6ef637cc2bbba3708cc27d96e2d3efd913315b24
AnandaCba <fabiogurudeva@gmail.com>
2020-10-07 19:47:17 -0300

    add

---
+++ challenges/3-area-trapezio/js/anandacba/areaTrapezio.js
@@ -0,0 +1,19 @@
+//Pegando input do user e transofrando um número.
+const userInput = process.argv.splice(2)
+const userInputToNumber = userInput.map(Number);
+
+function areaTrapezio(largeBase,smallBase,height){
+	//verificando os argumentos
+		for(let verifyIn of arguments){
+			if(isNaN(verifyIn)){return console.log("Error, please enter only type number.")}
+				if(typeof verifyIn !== 'number'){return console.log("Error, please enter only type number.")};
+		};
+		if (userInputToNumber.length !== 3){
+			return console.log("Please, enter 3 arguments!")
+		};
+	//Regra de negócio
+	const area = (largeBase + smallBase) * height / 2
+	console.log(`The area of trapeze is: ${area}`)
+};
+//Passando os argumentos para função
+areaTrapezio(...userInputToNumber)
---
+++ challenges/3-area-trapezio/js/anandacba/readme.md
@@ -0,0 +1,19 @@
+# Submissão de Exercicio
+
+**Exercicio:** 3 - Área de um trapézio
+
+**Nickname:** AnandaCba
+
+**Nível Técnico:** - Estudante/Junior -
+
+**Dificuldade de Resolução:** - Baixa -
+
+**Comentários:** 
+"O desafio desse exercício é o tratamento dos dados, sendo que o JavaScript considera 'NaN' tipo 'number'."
+
+**Como rodar o desafio**: 
+
+Use o comando abaixo: 
+```bash
+node areaTrapezio.js argv1'baseMaior' argv2'baseMenor' argv3'altura'
+```
