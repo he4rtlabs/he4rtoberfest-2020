9180085e9f46f3b5ae55a1ca1aba0c763ad965a8
mpgxc <mpgx5.c@gmail.com>
2020-10-07 15:31:45 -0300

    refact: input check and infinite input

--- challenges/1-media-alunos/c/mpgxc/main.c
+++ challenges/1-media-alunos/c/mpgxc/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "isvalid.h"
+
+#define buffer_size 256
 
 float media_alunos(char const *argv[], int size_arr)
 {
@@ -14,6 +19,36 @@ float media_alunos(char const *argv[], int size_arr)
   return media / size_arr;
 }
 
+float iterative_mode()
+{
+  int index = 0;
+  float media_notas = 0;
+  char buffer[buffer_size];
+
+  puts("Para finalizar o modo iterativo digite: q");
+
+  while (true)
+  {
+    printf("Digite um numero:");
+    scanf(" %s", buffer);
+
+    if (strcmp(buffer, "q") == 0)
+    {
+      break;
+    }
+    else if (checker(buffer))
+    {
+      media_notas += atof(buffer);
+      index += 1;
+    }
+    else
+    {
+      puts("Por favor digite numeros!");
+    }
+  }
+  return media_notas / index;
+}
+
 int main(int argc, char const *argv[])
 {
   if (argc == 1)
@@ -22,9 +57,15 @@ int main(int argc, char const *argv[])
     return EXIT_FAILURE;
   }
 
-  float media_notas = media_alunos(argv, argc - 1);
+  int input_size = argc - 1;
 
-  printf("Media-> %0.2f\n", media_notas);
+  if (check_input_isvalid(argv, input_size))
+  {
+    printf("Media-> %0.2f\n", media_alunos(argv, input_size));
+    return EXIT_SUCCESS;
+  }
+  puts("Uma entrada invalida ocorreu no CLI. Experimente o modo iteraivo.");
+  printf("Media-> %0.2f\n", iterative_mode());
 
   return EXIT_SUCCESS;
 }
