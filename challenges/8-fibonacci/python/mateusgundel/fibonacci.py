868ccb8cc6412643262c548661f9f68680468d23
Mateus Gündel <mateus8923@gmail.com>
2020-10-07 01:16:16 -0300

    Finalizado o Desafio

---
+++ challenges/8-fibonacci/python/mateusgundel/fibonacci.py
@@ -0,0 +1,24 @@
+import sys
+
+# Lê o número inteiro
+inteiro_N = int(sys.argv[1])
+
+# inicializa as variáveis de controle e a string de saída
+ultimo_numero = 1
+numero = 1
+string_resposta = ""
+
+for index in range(inteiro_N):
+    # Adiciona o 0 incial a saída
+    if index == 0:
+        string_resposta = string_resposta + f"0"
+    # Adiciona a saída o 1 inicial
+    elif index == 1:
+        string_resposta = string_resposta + f" {numero}"
+    else:
+        # Adiciona o número a saída e incrementa o último número e o número
+        string_resposta = string_resposta + f" {numero}"
+        numero, ultimo_numero = numero + ultimo_numero, numero
+
+# Exibe a saída
+print(string_resposta)
---
+++ challenges/8-fibonacci/python/mateusgundel/readme.md
@@ -0,0 +1,26 @@
+# Submissão de Exercicio
+
+**Exercicio:** 8 - Fibonacci
+
+**Nickname:** mateusgundel
+
+**Nível Técnico:** - Estudante/Programador Junior
+
+**Twitter**: https://twitter.com/mateus_gundel
+
+**Dificuldade de Resolução:** Baixa
+
+**Comentários:** Achei tranquilo de resolver, bastando entender que os dois primeiros números são fixos, os próximos 
+podem ser calculados facilmente durante a interação do for, bastando somente no for, realizar o print dos números com o 
+espaço que é solicitado no enuncia.
+
+**Como rodar o desafio**: 
+
+Use o comando abaixo: 
+```bash
+python fibonacci.py <inteiro N>
+```
+Por exemplo
+```bash
+python fibonacci.py 10
+```
