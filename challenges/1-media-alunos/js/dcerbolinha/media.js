3dfd41435bff896c447021d46f4360a465370946
Denys <denys@deckdesign.com.br>
2020-10-07 18:36:56 -0300

    Finalizado o desafio 01

---
+++ challenges/1-media-alunos/js/dcerbolinha/media.js
@@ -0,0 +1,15 @@
+function mediaAlunos(a, b){
+    console.log()
+    if(!a && !b){
+        return "As duas notas são obrigatórias"
+    }
+    if(isNaN(a)){
+        return "A primeira nota deve ser um numero"
+    }
+    if(isNaN(b)){
+        return "A segunda nota deve ser um numero"
+    }
+    return (a+b) / 2;
+}
+
+console.log(mediaAlunos(22,53))
