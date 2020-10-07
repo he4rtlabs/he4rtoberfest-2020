5ff8cd050435d841c1a495b1361d9582928cbf68
rafaelmendes1 <rarafafa3245@gmail.com>
2020-10-07 14:26:09 -0300

    Finalizando o desafio

---
+++ challenges/4-cifra-de-cesar/c++/rafaelmendes1/cifradecesar.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int cont, posicoesPular;
+
+  // Lista ACII
+  int letra;
+  string mensagem;
+
+  cin >> cont;
+  
+  while(cont>0){
+    cin >> mensagem;
+    cin >> posicoesPular;
+    cout << "Decifrado: ";
+    for(int i=0; i<mensagem.size();i++){
+      letra = toupper(mensagem[i]);
+      if(letra-posicoesPular <= 65){
+        letra += 16;
+        mensagem[i] = letra-posicoesPular;
+      }else {
+        mensagem[i] = letra-posicoesPular;
+      }
+      cout << mensagem[i];
+    }
+    cout << endl;
+    cont--;
+  }
+  
+}
