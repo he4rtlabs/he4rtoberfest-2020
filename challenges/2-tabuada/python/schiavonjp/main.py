5cfe8746fee7eade2d31652d6d9e527df1010aeb
João Paulo Schiavon <schiavonjp@yahoo.com.br>
2020-10-06 23:33:41 -0300

    Finalizando o desafio

---
+++ challenges/2-tabuada/python/schiavonjp/main.py
@@ -0,0 +1,17 @@
+import sys
+
+def process_item(item):
+    print('===========================')
+    print('A tabuada do numero {} é: '.format(item))
+    for number in range(1,11):
+        result = int(item)*int(number)
+        print('   {}*{} = {}'.format(item, number, int(result)))
+    
+    
+
+try:
+    number = int(sys.argv[1])
+    results = [process_item(item) for item in range(1,number+1)]
+    print('===========================')
+except Exception as e:
+    print(e)
