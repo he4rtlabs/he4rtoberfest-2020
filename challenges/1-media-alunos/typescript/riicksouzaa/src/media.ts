6089780b27ab8cf33b740b813b5f90a347035f84
Ricardo Souza <souzaariick@gmail.com>
2020-10-07 11:32:25 -0300

    Feature: [Implementada validação de números, permitido inserir mais de 2 notas para ser feita a média.]

--- challenges/1-media-alunos/typescript/riicksouzaa/src/media.ts
+++ challenges/1-media-alunos/typescript/riicksouzaa/src/media.ts
@@ -5,10 +5,38 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('Qual a primeira nota do aluno? ', (answer1) => {
-    rl.question('Qual a segunda nota do aluno? ', (answer2) => {
-        var result = ((+answer1) + (+answer2)) / 2;
-        console.log(`A média das notas é ${result}`);
-        rl.close();
-    });
-});
+let notas: number[] = []
+
+rl.question("\r\n\r\nDigite as notas dos alunos e caso queira finalizar digite apenas ENTER.\r\n", (answer: string) => {
+
+    if (isNumber(answer)) {
+        notas.push(parseInt(answer))
+    } else {
+        console.log(`${answer} não será somada para realização da meta pois não é um número`)
+    }
+
+    rl.on('line', (input) => {
+        if (input === "") {
+            const total = notas.reduce((soma, nt) => soma + nt)
+            const quantidade = notas.length
+
+            console.log(`A média das notas informadas é ${fazerMedia(total, quantidade)}.`)
+            rl.close()
+        } else if (isNumber(input)) {
+            notas.push(parseInt(input))
+        } else {
+            console.log(`${input} não será somada para realização da meta pois não é um número`)
+        }
+    })
+})
+
+function fazerMedia(total: number, quantidade: number) {
+    return total / quantidade;
+}
+
+
+function isNumber(value: string | number): boolean {
+    return ((value != null) &&
+        (value !== '') &&
+        !isNaN(Number(value.toString())))
+} 
