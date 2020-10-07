e4446f573dcbbc23c4e12668f159324d38fd966c
brenu <bvsousa64@gmail.com>
2020-10-07 12:26:39 -0300

    Modifica desafio 4

--- challenges/4-cifra-de-cesar/c/brenu/main.c
+++ challenges/4-cifra-de-cesar/c/brenu/main.c
@@ -1,31 +1,54 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char *caesarCipher(char *message, int shift){
-    int i;
+    char *result = malloc(50);
+    int i, initialValue;
 
     for(i=0;message[i]!='\0';i++){
+        initialValue = message[i];
         message[i] = message[i] - shift;
         if(message[i] < 65) {
             message[i] = 91 - (65 - message[i]);
+        }else if(initialValue >= 97 && message[i] < 97){
+            message[i] = 123 - (97 - message[i]);
         }
     }
 
-    return message;
+    strcpy(result, message);
+    return result;
 }
 
 int main(){
     int i, testCases, shift;
-    char *initialMessage, *resultMessage;
+    char *initialMessage, *resultMessage, **results;
+    initialMessage = malloc(50);
+    resultMessage = malloc(50);
 
-    initialMessage = malloc(50*sizeof(char));
-    resultMessage = malloc(50*sizeof(char));
-
+    printf("Cifra de César\n");
+    printf("[*] A primeira linha de entrada corresponde ao número de casos de teste\n");
+    printf("[*] Para cada caso, informe a sentença para ser codificada\n");
+    printf("[*] Informe também o numero de posições que cada letra sera deslocada\n");
     scanf("%d", &testCases);
-    for(i=1 ; i<=testCases ; i++){
+
+    results = malloc(testCases);
+    for(i=0;i<testCases;i++){
+        results[i] = malloc(50);
+        if (results[i]==NULL){
+            printf("Você não tem memória o suficiente no sistema. Essa aplicação será encerrada\n");
+            exit(1);
+	    }
+    }
+
+    for(i=0 ; i<testCases ; i++){
         scanf("%s", initialMessage);
         scanf("%d", &shift);
-        resultMessage = caesarCipher(initialMessage, shift);
-        printf("%s\n", resultMessage);
+        results[i] = caesarCipher(initialMessage, shift);
     }
+
+    for(i=0;i<testCases;i++){
+        printf("Decifrado: %s\n", &results[i][0]);
+    }
+    free(results);
 }
