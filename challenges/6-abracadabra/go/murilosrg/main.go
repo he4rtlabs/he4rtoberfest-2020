3f8ad4cfce5fd20efe151f6ea9b5d3a9a0abc577
murilosrg <muriilo@live.it>
2020-10-07 07:36:51 -0300

    limita tamanho palavra

--- challenges/6-abracadabra/go/murilosrg/main.go
+++ challenges/6-abracadabra/go/murilosrg/main.go
@@ -6,14 +6,27 @@ import (
 )
 
 func main() {
-	var entrada string
-	fmt.Print("Informe a palavra: ")
-	fmt.Scan(&entrada)
+	entrada := lerPalavra()
 	palavra := strings.Split(entrada, "")
 
 	for i := 0; i < len(entrada); i++ {
 		fmt.Println(strings.Join(palavra, " "))
 		palavra = append([]string{""}, palavra...)
 		palavra = palavra[:len(palavra)-1]
+	}
+}
+
+func lerPalavra() string {
+	for {
+		var entrada string
+		fmt.Print("Informe a palavra: ")
+		fmt.Scan(&entrada)
+
+		if len(entrada) < 2 || len(entrada) > 100 {
+			fmt.Print("entrada invalida, deve conter entre 2 e 100 caracteres \n")
+			return lerPalavra()
+		}
+
+		return entrada
 	}
 }
