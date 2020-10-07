800ca58779db7e43eb4c69010e8c06cfef31276d
AnandaCba <fabiogurudeva@gmail.com>
2020-10-07 19:08:47 -0300

    master

---
+++ challenges/1-media-alunos/js/anandacba/mediaAlunos.js
@@ -0,0 +1,18 @@
+//Pode enviar diversos valores para argumento
+function calcAverage(inputNumber){
+	//validação do tipo
+	for (let verifyIn of arguments) {
+		if (typeof verifyIn !== 'number'){
+			return console.log("Values are not number!")
+		};
+	};
+	//aritmética
+	let sumNumber = 0;
+	for (let count = 0; count <= arguments.length -1; count ++){	
+		sumNumber = sumNumber + arguments[count]
+	};
+	const average = sumNumber / arguments.length
+	console.log("The average of the values is: " + average)
+};
+//Passando os valores para dentro da função
+calcAverage(22,53);
---
+++ challenges/1-media-alunos/js/anandacba/readme.md
@@ -0,0 +1,21 @@
+# Submissão de Exercicio
+
+**Exercicio:** 1 - Média Alunos
+
+**Nickname:** AnandaCba
+
+**Nível Técnico:** - Estudante/Junior -
+
+**Dificuldade de Resolução:** - Baixa -
+
+**Comentários:** 
+
+"O desafio em si é simples, a parte de escrever o mais claro, limpo e
+objetivo possível que acredito ser o desafio."
+
+**Como rodar o desafio**: 
+
+Use o comando abaixo: 
+```bash
+node mediaAlunos.js
+```
