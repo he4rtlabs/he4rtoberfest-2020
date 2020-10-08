f2ae92b312cda3f6abb1d27715d52bc1b7dd97ab
Leticia Caroline Leonardo <leticiacaroline-123@hotmail.com>
2020-10-08 20:27:37 -0300

    Finalizando o desafio

---
+++ challenges/2-tabuada/c/levxyca/README.md
@@ -0,0 +1,18 @@
+# Submissão de Exercicio
+
+**Exercicio:** 2 - Tabuada
+
+**Nickname:** levxyca
+
+**Nível Técnico:** - Estudante
+
+**Twitter**: https://twitter.com/levxyca
+
+**Dificuldade de Resolução:** - Baixa
+
+**Como rodar o desafio**: 
+
+Use o comando abaixo: 
+```bash
+gcc tabuada.c -o tabuada && ./tabuada
+```
---
+++ challenges/2-tabuada/c/levxyca/tabuada.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("\n::::::::::::: TABUADA:::::::::::::\n");
+
+    printf("Você deseja ver a tabuada até qual número?\n");
+    scanf("%d", &numero);
+
+    for(int i = 0; i <= numero; i++) {
+        printf("\n~===========================~\n");
+        printf("A tabuada do número %d é:\n", i);
+        for(int j = 0; j <= 10; j++) {
+            printf("%d * %d = %d\n", i, j, i*j);
+        }
+        ("\n~===========================~\n");
+    }
+    return 0;
+}
