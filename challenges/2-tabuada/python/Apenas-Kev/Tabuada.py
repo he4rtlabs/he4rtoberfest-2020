390aa058a2fe395bac8b63a821d4ad52c49217f0
Kelwin Karan <kelwinkaran@gmail.com>
2020-10-08 17:59:29 -0300

    código base

---
+++ challenges/2-tabuada/python/Apenas-Kev/Tabuada.py
@@ -0,0 +1,5 @@
+x = int(input('Insira o numero: '))
+print('='*27)
+while (x > 0):
+    print(f'A tabuada do numero {x} é:\n  {x}*1 = {x}\n  {x}*2 = {x*2}\n  {x}*3 = {x*3}\n  {x}*4 = {x*4}\n  {x}*5 = {x*5}\n  {x}*6 = {x*6}\n  {x}*7 = {x*7}\n  {x}*8 = {x*8}\n  {x}*9 = {x*9}\n  {x}*10 = {x*10}\n{"="*27}')
+    x = x - 1
