e39a0f001e907d0817a72d5830e0a412cabd4f24
Clerivaldo Junior <clerivaldoj@gmail.com>
2020-10-07 21:18:38 -0300

    Tenho fé que agora vai

--- challenges/4-cifra-de-cesar/python/clerijr/main.py
+++ challenges/4-cifra-de-cesar/python/clerijr/main.py
@@ -1,24 +1,33 @@
 # -*- coding: utf-8 -*-
 def main():
-    while True:
-        cifra = input("Digite sua cifra: ")
-        try:
-            cifraPos = int(input("Digite quantas posições para decodificar: "))
-        except ValueError:
-            print("Você digitou um valor inválido! Apenas números!")
-            break;
-        else:
-            decoded = '';
-            for char in cifra:
-                if (ord(char) - cifraPos) < 65:
-                    char = chr((ord(char)-cifraPos)+26)
-                else:
-                    char = chr((ord(char)-cifraPos))
-                decoded+=char;
-            print("Decifrado: {} \n".format(decoded));
-        buffer = input("Digite \"sair\" para sair do programa, qualquer outro valor para repetir\n");
-        if buffer.upper() == "SAIR":
-            break;
+    try:
+        caseNum = int(input("Digite o número de casos: "))
+    except ValueError:
+        print("Digite um valor válido!")
+    else:  
+        while caseNum > 0:
+            cifra = input("Digite sua cifra com até 50 caracteres: ")
+            if len(cifra) > 50:
+                print("Digite uma mensagem com até 50 caracteres!")
+                break
+            cifra.upper()
+            try:
+                cifraPos = int(input("Digite quantas posições para decodificar, de 0 a 25: "))
+                while cifraPos < 0 or cifraPos > 25:
+                    cifraPos = int(input("Digite um número válido entre 0 e 25: "))
+            except ValueError:
+                print("Você digitou um valor inválido! Apenas números!")
+                break;   
+            else:
+                decoded = ''
+                for char in cifra:
+                    if (ord(char) - cifraPos) < 65:
+                        char = chr((ord(char)-cifraPos)+26)
+                    else:
+                        char = chr((ord(char)-cifraPos))
+                    decoded+=char
+                print("Decifrado: {}".format(decoded));
+                print("="*30)
+                caseNum = caseNum - 1
 if __name__ == "__main__":
     main()
-    
--- challenges/4-cifra-de-cesar/python/clerijr/readme.md
+++ challenges/4-cifra-de-cesar/python/clerijr/readme.md
@@ -10,14 +10,14 @@
 
 **Dificuldade de Resolução:** Média
 
-**Gostaria ainda de adicionar:** Um exercício muito bom, que acredito que existam diversas outras formas de realizá-lo. Utilizei uma estrutura de if para dar a impressão de ciclar apenas pelo alfabeto, porém também converterá demais caracteres ascii. Qualquer sinalização de bugs ou alterações recomendadas são muito bem vindas.
+**Gostaria ainda de adicionar:** Um exercício muito bom, que acredito que existam diversas outras formas de realizá-lo. Utilizei uma estrutura de if para dar a impressão de ciclar apenas pelo alfabeto, porém também converterá demais caracteres ascii. Qualquer sinalização de bugs ou alterações recomendadas são muito bem vindas. Devido ao input com o número de posições, achei desnecessário colocar uma opção para sair no final do código.
 
 **Como rodar o desafio**: 
 
 Use o comando abaixo: 
 ```bash
 python main.py;
+digitar o numero de casos,
 digitar a cifra desejada,
-digitar o número de posições,
-se quiser sair, digitar 'sair', caso contrário, pressionar enter
+digitar o número de posições
 ```
