52c4855355c9debde46d249fc0b39c772412cef0
moisesabraaoso <moisesabraaoso@gmail.com>
2020-10-08 01:46:06 -0300

    Adicionando validação para aceitar apenas números

--- challenges/3-area-trapezio/python/moisesabraao/app.py
+++ challenges/3-area-trapezio/python/moisesabraao/app.py
@@ -1,9 +1,13 @@
 #encoding:utf8
 
-baseMaior = float(input('Informe o valor da base maior do trapézio: '))
-baseMenor = float(input('Informe o valor da base menor do trapézio: '))
-altura = float(input('Informe o valor da altura do trapézio: '))
-
-resultado = ((baseMaior + baseMenor) * altura) / 2
-
-print("A área do trapézio é: ", resultado)
+while True:
+    try:
+        baseMaior = float(input('Informe o valor da base maior do trapézio: '))
+        baseMenor = float(input('Informe o valor da base menor do trapézio: '))
+        altura = float(input('Informe o valor da altura do trapézio: '))
+    except ValueError:
+        print("Valor inválido! Informe apenas números!")
+    else:
+        resultado = ((baseMaior + baseMenor) * altura) / 2
+        print("A área do trapézio é: ", resultado)
+        break
