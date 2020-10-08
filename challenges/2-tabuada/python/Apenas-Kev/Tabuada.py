344f2f5252e2f7b2646c15013d0b24a5850a3cdc
Kelwin Karan <kelwinkaran@gmail.com>
2020-10-08 19:53:44 -0300

    Programa refeito
    Agora utilizando 'for' em vez de 'while'

--- challenges/2-tabuada/python/Apenas-Kev/Tabuada.py
+++ challenges/2-tabuada/python/Apenas-Kev/Tabuada.py
@@ -3,10 +3,10 @@ while True:
         numero = int(input('Insira o numero para receber a tabuada: '))
         break;
     except ValueError:
-        print('Por favor, insira apenas numeros inteiros')
-ctrl = 0
-print('='*27)
-while (numero > 0):
-    ctrl = (ctrl + 1)
-    print(f'A tabuada do numero {ctrl} é:\n  {ctrl}*1 = {ctrl}\n  {ctrl}*2 = {ctrl*2}\n  {ctrl}*3 = {ctrl*3}\n  {ctrl}*4 = {ctrl*4}\n  {ctrl}*5 = {ctrl*5}\n  {ctrl}*6 = {ctrl*6}\n  {ctrl}*7 = {ctrl*7}\n  {ctrl}*8 = {ctrl*8}\n  {ctrl}*9 = {ctrl*9}\n  {ctrl}*10 = {ctrl*10}\n{"="*27}')
-    numero = numero - 1
+        print(f'{"-" * 13} ERRO {"-" * 13 }\nInsira somente numeros inteiros.\n')
+print(f'\n{"="*27}')
+for var in range(1, numero + 1):
+    print(f'A tabuada do numero {var} é:')
+    for tabuada in range(1,11):
+        print(f'  {var} * {tabuada} = {var * tabuada}')
+    print('=' * 27)
