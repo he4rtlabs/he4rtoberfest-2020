52d44e3b4b28a6b0093cb22bebd5abc8d1409b09
Felipe Ribeiro <felipe.engcontrole@gmail.com>
2020-10-08 08:11:36 -0300

    Desafio 1

---
+++ challenges/1-media-alunos/cpp/feliper2002/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void calculaMedia(int nota1, int nota2) {
+    float media = (nota1+nota2)/2;
+    printf("%.1f", media);
+}
+
+int main() {
+    int nota1, nota2; cin >> nota1 >> nota2;
+    return 0;
+}
