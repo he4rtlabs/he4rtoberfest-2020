c0269d638fadf8579517a7b26e0f6705fe52cf70
ThesllaDev <theslladev@gmail.com>
2020-10-09 15:23:31 -0300

    Finalizando o desafio

--- challenges/1-media-alunos/js/ThesllaDev/media-alunos.js
+++ challenges/1-media-alunos/js/ThesllaDev/media-alunos.js
@@ -1,9 +1,9 @@
 const nota1 = 22,
     nota2 = 53;
 
-function calcularMedia(nota1, nota2) {
+function calculaMedia(nota1, nota2) {
     var media = (nota1 + nota2) / 2;
     return media;
 }
 
-console.log("Média -> " + calcularMedia(nota1, nota2));
+console.log("Média -> " + calculaMedia(nota1, nota2));
