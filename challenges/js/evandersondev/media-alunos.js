b9c0d99ef30df9c002c46a9536233fe10ecf4943
evandersondev <evandersonvasconcelos23@gmail.com>
2020-10-09 12:59:42 -0300

    Finalizando o desafio

---
+++ challenges/js/evandersondev/media-alunos.js
@@ -0,0 +1,9 @@
+const student1 = 22
+const student2 = 53
+
+function media(note1, note2) {
+  const result = (note1 + note2) / 2 
+  console.log(`Media -> ${result}`)
+}
+
+media(student1, student2)
