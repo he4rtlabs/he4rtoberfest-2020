142b74a0e22c62ae42b9dd085dd47c330516939b
Leticia Caroline Leonardo <leticiacaroline-123@hotmail.com>
2020-10-09 10:06:22 -0300

    Arrumando o desafio

--- challenges/2-tabuada/c/levxyca/tabuada.c
+++ challenges/2-tabuada/c/levxyca/tabuada.c
@@ -8,10 +8,10 @@ int main() {
     printf("Você deseja ver a tabuada até qual número?\n");
     scanf("%d", &numero);
 
-    for(int i = 0; i <= numero; i++) {
+    for(int i = 1; i <= numero; i++) {
         printf("\n~===========================~\n");
         printf("A tabuada do número %d é:\n", i);
-        for(int j = 0; j <= 10; j++) {
+        for(int j = 1; j <= 10; j++) {
             printf("%d * %d = %d\n", i, j, i*j);
         }
         ("\n~===========================~\n");
