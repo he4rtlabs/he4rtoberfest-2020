46ccf0c2163fad32d04bfbe44395a9cfdc90e807
Arthur Maurício <arthur.mauricio@academico.ifpb.edu.br>
2020-10-09 09:04:37 -0300

    adicionando exceções

--- challenges/8-fibonacci/python/arthur-mts/main.py
+++ challenges/8-fibonacci/python/arthur-mts/main.py
@@ -1,8 +1,10 @@
 def fibo(n):
     ns = [0]
     res = "0"
-    if((n <= 1) or (n > 46)):
+    if(n == 1):
         return res
+    elif((n < 1) or (n > 46)):
+        raise Exception("O número deve ser maior que 0 e menor que 46")
     else:
         for i in range(1, n):
             if (i == 1):
@@ -12,7 +14,6 @@ def fibo(n):
                 v = (ns[i - 2] + ns[i - 1])
                 ns.append(v)
                 res += str(v) + " "
-        res = res[:-1]
         return res
 
 
