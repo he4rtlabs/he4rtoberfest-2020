b9f9104c71909c7d80dc0a8a1f69e770e6729607
Thuan <thuansilva14@gmail.com>
2020-10-09 20:22:14 -0400

    Finalizando o desafio

---
+++ challenges/0-hello-world/c/thuansilva/helloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+    printf("\n\tHello He4rtoberfest!\nMe chamo Thuan e estou muito feliz por participar desse evento!\n");
+    return 0;
+}
