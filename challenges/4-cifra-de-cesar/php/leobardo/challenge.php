1a64e4f0a3e16fcf1ae4495761479431e78c915b
LeoBardo <45073941+LeoBardineo@users.noreply.github.com>
2020-10-13 19:34:51 -0300

    Finalizando desafio.

---
+++ challenges/4-cifra-de-cesar/php/leobardo/challenge.php
@@ -0,0 +1,49 @@
+<?php
+
+try {
+    if (!is_numeric($argv[1])) {
+        throw new Exception('O número de casos deve ser um número. (dã ?)');
+    }
+
+    $numeroCasos = (int)$argv[1];
+
+    echo 'ENTRADAS' . PHP_EOL . PHP_EOL;
+    for ($i = 0; $i < $numeroCasos; $i++) {
+        echo 'Entrada número ' . ($i + 1) . PHP_EOL;
+        $mensagens[] = readline('Digite a mensagem: ');
+
+        if (!ctype_alpha($mensagens[$i])) {
+            throw new Exception('Digite apenas letras do alfabeto na mensagem.');
+        }
+
+        $numeroPosicoes[] = readline('Digite a quantidade de posições que deve ser pulada: ');
+
+        if (!is_numeric($numeroPosicoes[$i])) {
+            throw new Exception('A quantidade de posições a ser pulada deve ser um número.');
+        }
+        echo PHP_EOL;
+    }
+
+    echo 'SAÍDAS' . PHP_EOL . PHP_EOL;
+    for ($i = 0; $i < $numeroCasos; $i++) {
+        echo 'Saída número ' . ($i + 1) . PHP_EOL;
+        $numeroPosicao = (int)$numeroPosicoes[$i];
+        $mensagem = str_split(strtoupper($mensagens[$i]));
+
+        array_map(function ($element) use ($numeroPosicao) {
+            $ordLetra = ord($element);
+            $calcOrd = $ordLetra - $numeroPosicao;
+
+            if ($calcOrd < 65) {
+                $numeroPosicao = abs($calcOrd - 64);
+                $calcOrd = abs($numeroPosicao - 90);
+            }
+
+            $novaLetra = chr($calcOrd);
+            echo $novaLetra;
+        }, $mensagem);
+        echo PHP_EOL . PHP_EOL;
+    }
+} catch (Exception $e) {
+    echo 'Por favor, tente novamente. '.$e->getMessage();
+}
---
+++ challenges/4-cifra-de-cesar/php/leobardo/readme.md
@@ -0,0 +1,20 @@
+# Submissão de Exercicio
+
+**Exercicio:** 4 - Cifra de César
+
+**Nickname:** leobardo
+
+**Nível Técnico:** - Estudante
+
+**Twitter**: https://twitter.com/lbardo0 (opcional)
+
+**Dificuldade de Resolução:** - Difícil
+
+**Comentários:** Nesse desafio eu achei que sim.
+
+**Como rodar o desafio**: 
+
+Use o comando abaixo: 
+```bash
+php challenge.php numeroPosicoes mensagem
+```
