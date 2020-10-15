36fdbbd76d162aec24862e6e8aeb22e643bdcb9e
Alba <alba__22@outlook.com>
2020-10-15 00:41:50 -0300

    Adições ao desafio

--- challenges/1-media-alunos/dart/Alba-22/average.dart
+++ challenges/1-media-alunos/dart/Alba-22/average.dart
@@ -1,9 +1,22 @@
 import 'dart:io';
 
 void main() {
-  String input = stdin.readLineSync().trim();
-  int firstGrade = int.parse(input.split(" ")[0].trim());
-  int secondGrade = int.parse(input.split(" ")[1].trim());
-  double average = (firstGrade + secondGrade) / 2;
-  stdout.write("Média -> $average");
+  try {
+    String input = stdin.readLineSync().trim();
+    List<String> grades = input.split(RegExp(r"(\s+)"));
+    double average, sum = 0;
+    for (String grade in grades) {
+      sum += double.parse(grade);
+    }
+    average = sum / grades.length;
+    stdout.write("Média -> $average");
+  }
+  catch (error) {
+    if (error.runtimeType == FormatException) {
+      print("Input inválido");
+    }
+    else {
+      print("Ocorreu um erro");
+    }
+  }
 }
