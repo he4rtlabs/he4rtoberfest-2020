118b7b6b97ad931403a7e95eb7fea89b9cca5764
Priscilla P <passaropriscilla@gmail.com>
2020-10-21 00:36:02 -0300

    Finalizando o desafio 1

---
+++ challenges/1-media-alunos/c/pBird/mediaAlunos.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double media, nota1, nota2;
+    nota1 = 22;
+    nota2 = 53;
+
+    media = (nota1 + nota2)/2;
+
+    printf("A MEDIA é = %.1lf\n", media);
+    return 0;
+}
